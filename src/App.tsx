fb8a6af097d75c0bdcd4dac74f1c1a505ed79d99
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-09-24 22:49:28 +0530

    magic ui installed --major

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MarqueeDemo } from './components/ui/MarqueeDemo'
+
+function App() {
+  return (
+    <div>
+           <MarqueeDemo />
+    </div>
+  )
+}
+
+export default App
