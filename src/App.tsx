0861ffef94bbbfaa05c8873bc8db70baad6e789a
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-09-25 17:04:30 +0530

    section 3 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Hero from './components/Section1/Hero/Hero'
 import Section2 from './components/Section2/Section2'
+import Section3 from './components/Section3/Section3'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
            {/* <MarqueeDemo /> */}
            <Hero/>
            <Section2 />
+           <Section3 />
     </div>
   )
 }
---
+++ src/components/Section3/Section3.tsx
@@ -0,0 +1,10 @@
+
+function Section3() {
+  return (
+    <div className='sec3'>
+      
+    </div>
+  )
+}
+
+export default Section3
