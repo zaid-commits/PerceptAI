3a857cebca9f68079ecbf88a19c5fa848942288b
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-09-27 19:39:23 +0530

    section 3 removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Hero from './components/Section1/Hero/Hero'
 import Section2 from './components/Section2/Section2'
-import Section3 from './components/Section3/Section3'
 
 function App() {
   return (
@@ -8,7 +7,6 @@ function App() {
            {/* <MarqueeDemo /> */}
            <Hero/>
            <Section2 />
-           {/* <Section3 /> */}
     </div>
   )
 }
--- src/components/Section3/Section3.tsx
+++ src/components/Section3/Section3.tsx
@@ -3,6 +3,7 @@ function Section3() {
   return (
     <div className='sec3'>
       <div className="waitlist">
+        hello
       </div>
     </div>
   )
