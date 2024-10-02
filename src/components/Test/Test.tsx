43e3a56586c0d337c19023ddfac3b6479f67ffef
Gabbar Scripts <gabbarframework@gmail.com>
2024-10-02 19:03:00 +0530

    "Updated App.tsx to use react-router-dom, modified Navbar.tsx to use Link component.

---
+++ src/components/Test/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Test: React.FC = () => {
+    return (
+        <div>
+            <h1>Test Page</h1>
+            <p>This is a simple test page.</p>
+        </div>
+    );
+};
+
+export default Test;
