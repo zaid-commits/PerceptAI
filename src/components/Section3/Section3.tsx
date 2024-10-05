8cf5ce265be69faf240963a9f5b7cc26b15c7687
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-10-05 11:53:22 +0530

    glass card added

--- src/components/Section3/Section3.tsx
+++ src/components/Section3/Section3.tsx
@@ -1,10 +1,11 @@
 import { AnimatedBeamMultipleOutputDemo } from "../magicui/AnimatedBeamMultipleOutputDemo"
-
+import GlassContainer from "./GlassCard"
 function Section3() {
   return (
     <div className='sec3'>
       <div className="multiContainer">
         <AnimatedBeamMultipleOutputDemo />
+      <GlassContainer />
       </div>
     </div>
   )
