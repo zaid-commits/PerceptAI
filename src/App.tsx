99c2be10ee6aa44546bbed4cc593f1f158007011
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-11-17 23:43:28 +0530

    vercel analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { Main } from './components/Section1/Hero/Main'
 import Test from './components/Test/Test'
 import ReactLenis from 'lenis/react'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
