3649f4298974351587cffa564aa6f3da33394b4d
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-11-17 19:58:10 +0530

    refactered the layout of the file

--- src/components/Section1/Hero/Main.tsx
+++ src/components/Section1/Hero/Main.tsx
@@ -2,15 +2,15 @@ import Hero from "@/components/Section1/Hero/Hero";
 import Section2 from "@/components/Section2/Section2";
 import Section3 from "@/components/Section3/Section3";
 import Footer from "@/components/Footer";
-import VideoTab from "@/components/Section4/video-tabs";
+import Section4 from "@/components/Section4/Section4";
 
 export function Main() {
     return(
         <div>
             <Hero />
+            <Section4/>
             <Section2 />
             <Section3 />
-            <VideoTab />
             <Footer />
         </div>
     )
---
+++ src/components/Section4/Section4.tsx
@@ -0,0 +1,11 @@
+import VideoTab from './video-tabs'
+
+function Section4() {
+  return (
+    <div className='bg-black'>
+      <VideoTab />
+    </div>
+  )
+}
+
+export default Section4
