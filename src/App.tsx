7e323d33e842b16ba0783b9977f8bc5de5cba85c
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-11-17 22:36:42 +0530

    implemented smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+'use client'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { Main } from './components/Section1/Hero/Main'
 import Test from './components/Test/Test'
+import ReactLenis from 'lenis/react'
 
 function App() {
   return (
+    <ReactLenis root>
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/Test" element={<Test />} />
       </Routes>
     </Router>
+    </ReactLenis>
   )
 }
 
