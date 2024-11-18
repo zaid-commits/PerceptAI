39c61178ab3750d197b7973c99c07f9851bc7fbc
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-11-18 21:43:28 +0530

    feat: add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 'use client'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { Main } from './components/Section1/Hero/Main'
-import Test from './components/Test/Test'
+import Test from './components/pages/Test'
+import Section2 from './components/Section2/Section2'
+import Section5 from './components/Section5/Section5'
+import Section8 from './components/Section8/ResourceLibrary'
 import ReactLenis from 'lenis/react'
 import { Analytics } from "@vercel/analytics/react"
+import CommunityForum from './components/Section9/CommunityForum'
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/Projects" element={<Section2 />} />
+        <Route path="/Resources" element={<Section8 />} />
+        <Route path='/Community' element={<CommunityForum/>}/>
+        <Route path="/Contact" element={<Section5 />} />
         <Route path="/Test" element={<Test />} />
       </Routes>
     </Router>
