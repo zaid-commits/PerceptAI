abe92c90a9e36b43d2a23a4e3f9bf97c5ec4b179
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-11-18 22:09:17 +0530

    inc: new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 'use client'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { Main } from './components/Section1/Hero/Main'
-import Test from './components/pages/Test'
-import Section2 from './components/Section2/Section2'
-import Section5 from './components/Section5/Section5'
-import Section8 from './components/Section8/ResourceLibrary'
+import Test from './components/routes/Test'
 import ReactLenis from 'lenis/react'
 import { Analytics } from "@vercel/analytics/react"
-import CommunityForum from './components/Section9/CommunityForum'
-
+import Projects from './components/routes/Projects'
+import Community from './components/routes/Community'
+import Resources from './components/routes/Resources'
+import Contact from './components/routes/Contact'
 function App() {
   return (
     <ReactLenis root>
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/Projects" element={<Section2 />} />
-        <Route path="/Resources" element={<Section8 />} />
-        <Route path='/Community' element={<CommunityForum/>}/>
-        <Route path="/Contact" element={<Section5 />} />
+        <Route path="/Projects" element={<Projects/>} />
+        <Route path="/Resources" element={<Resources/>} />
+        <Route path='/Community' element={<Community/>}/>
+        <Route path="/Contact" element={<Contact />} />
         <Route path="/Test" element={<Test />} />
       </Routes>
     </Router>
