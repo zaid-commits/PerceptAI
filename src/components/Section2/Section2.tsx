74914801171f43fc4c398c1d7618180bcf8998b5
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-11-18 21:44:22 +0530

    add: new route

--- src/components/Section2/Section2.tsx
+++ src/components/Section2/Section2.tsx
@@ -4,10 +4,12 @@ import Sidebar from './SideBar/SideBar';
 import ProjectGrid from './ProjectGrid/ProjectGrid';
 import './Section2.css';
 import '../../App.css';
+import FloatingNavbar from '../Section1/Navbar/Navbar';
 
 const Section2: React.FC = () => {
   return (
     <div className="root flex">
+      <FloatingNavbar/>
       <Sidebar />
 
       <div className="w-4/5 p-4">
