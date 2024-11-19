243f1552e3278e18f04b8e915f1af00267e478e1
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-11-19 21:13:03 +0530

    feat: auth routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Route, BrowserRouter as Router, Routes, useLocation } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes, useLocation, Navigate } from 'react-router-dom';
 import { Main } from './components/Section1/Hero/Main';
 import Test from './components/routes/Test';
 import { Analytics } from "@vercel/analytics/react";
@@ -7,13 +7,17 @@ import Projects from './components/routes/Projects';
 import Community from './components/routes/Community';
 import Resources from './components/routes/Resources';
 import Contact from './components/routes/Contact';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import UserProfile from './components/Auth/Profile';
 import LoadingSpinner from './components/LoadingSpinner';
 import { LoadingProvider, useLoading } from './context/LoadingContext';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const AppRoutes: React.FC = () => {
   const { setLoading } = useLoading();
   const location = useLocation();
+  const [isLoggedIn] = useState(false);
 
   useEffect(() => {
     setLoading(true);
@@ -32,6 +36,9 @@ const AppRoutes: React.FC = () => {
       <Route path="/Community" element={<Community />} />
       <Route path="/Contact" element={<Contact />} />
       <Route path="/Test" element={<Test />} />
+      <Route path="/Register" element={isLoggedIn ? <Navigate to="/UserProfile" /> : <Register />} />
+      <Route path="/Login" element={isLoggedIn ? <Navigate to="/UserProfile" /> : <Login />} />
+      <Route path="/UserProfile" element={isLoggedIn ? <UserProfile /> : <Navigate to="/Login" />} />
     </Routes>
   );
 };
