cb1921c7452935dadd81fc055d837f6b2dd8b03c
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-11-19 08:37:28 +0530

    upd: background image changed

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -34,9 +34,9 @@ const LoadingSpinner: React.FC = () => {
   if (!loading) return null; 
 
   return (
-    <div className={`flex flex-col justify-center items-center h-screen ${animation ? 'bg-gradient-to-r from-purple-600 to-pink-600' : 'bg-black'} ${animation ? 'animate-pulse' : ''}`} style={{ backgroundImage: `url('https://i.pinimg.com/originals/ee/6e/d5/ee6ed5945d444fb5046149386acec4c8.gif')`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div className={`flex flex-col justify-center items-center h-screen ${animation ? 'bg-gradient-to-r from-purple-600 to-pink-600' : 'bg-black'} ${animation ? 'animate-pulse' : ''}`} style={{ backgroundImage: `url('https://i.pinimg.com/originals/ee/6e/d5/ee6ed5945d444fb5046149386acec4c8.gif')`, backgroundSize: 'contain', backgroundPosition: 'center' }}>
       <div className={`rounded-full h-32 w-32 border-t-4 border-b-4 border-purple-600 ${animation ? 'animate-none' : 'animate-spin'}`}></div>
-      <p className={`text-white mt-4 text-2xl font-bold ${animation ? 'animate-pulse' : ''}`}>{text}</p>
+      <p className={`text-white mt-4 text-4xl font-mono font-bold ${animation ? 'animate-pulse' : ''}`}>{text}</p>
     </div>
   );
 };
