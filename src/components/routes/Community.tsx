809471daccaf6df84d2182ea1216a804aee02eca
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-12-03 14:56:07 +0000

    old component added

--- src/components/routes/Community.tsx
+++ src/components/routes/Community.tsx
@@ -1,12 +1,11 @@
-import CommunityContainer from "../Community/Container"
-
+import CommunityForum from "../Community/Forum"
 import FloatingNavbar from "../Section1/Navbar/Navbar"
 
 function Community() {
   return (
     <div>
         <FloatingNavbar/>
-        <CommunityContainer/>
+        <CommunityForum/>
     </div>
   )
 }
