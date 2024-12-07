b2a697c6486dd296fb90ba78c723d5359a0f7180
Zaid <zaidcommits.github@gmail.coom>
2024-12-07 14:40:06 +0530

    deployment test

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { ClerkProvider } from '@clerk/clerk-react';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
+  throw new Error("Missing Publishable Key")
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -16,4 +17,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />
     </ClerkProvider>
   </React.StrictMode>,
-);
+)
