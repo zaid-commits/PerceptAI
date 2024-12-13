856deff190ae48227a60ebf4367bd9d2cd6e3868
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-12-13 16:39:53 +0530

    add: toast for spin up ser notification

--- src/components/routes/Community.tsx
+++ src/components/routes/Community.tsx
@@ -2,9 +2,12 @@ import { useEffect } from 'react'
 import FloatingNavbar from "../Section1/Navbar/Navbar"
 // import Footer from "../Footer"
 import Chat from '../Community/Chat'
+import { Toaster, toast } from 'react-hot-toast'
+
 const Community = () => {
   useEffect(() => {
     document.body.style.backgroundColor = 'black'
+    toast("Chat server is spinning up! Please wait ~50 seconds as we are using a free instance for our backend server.", { duration: 50000 })
     return () => {
       document.body.style.backgroundColor = ''
     }
@@ -16,7 +19,9 @@ const Community = () => {
       <main className="flex-grow p-4 md:p-8 pt-24 pb-16 flex justify-center items-center">
         <Chat />
       </main>
+      
       {/* <Footer /> */}
+      <Toaster position='bottom-right'/>
     </div>
   )
 }
