fda54b42e5d781359d1ff670191b8164f5c5e136
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-12-25 11:08:19 +0530

    cmt : posterimage

--- src/components/Resources/AllResources.tsx
+++ src/components/Resources/AllResources.tsx
@@ -60,44 +60,47 @@ const AllResources: React.FC = () => {
           className="block w-full mb-2 p-2 rounded bg-black text-white border border-gray-800 focus:outline-none focus:ring-2 focus:ring-purple-600"
         />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {filteredResources.map(resource => (
-            resource.title && resource.description && resource.link && resource.category && (
-              <div
-                key={resource._id}
-                className="bg-black border-b border-l border-purple-800 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow"
-              >
-                <h3 className="text-xl font-bold text-white mb-2">{resource.title}</h3>
-                <p className="text-gray-400 mb-4">{resource.description}</p>
-                <a
-                  href={resource.link}
-                  className="text-purple-400 hover:text-purple-300 transition-colors"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  View full resource
-                </a>
-                <div className="mt-4 flex  items-center">
-                  <span className="inline-block bg-purple-700 text-white text-xs px-2 py-1 rounded">
-                    {resource.category}
-                  </span>
-                  {resource.posterImage && (
-                    <img
-                      className="rounded-lg w-10 h-10 ml-4"
-                      src={resource.posterImage}
-                      alt={resource.posterUsername}
-                    />
-                  )}
-                  <span className="text-white ml-2">
-                    posted by <a href="#" className="text-purple-800">{resource.posterUsername}</a>
-                  </span>
+                    {filteredResources.map(resource => (
+                      resource.title && resource.description && resource.link && resource.category && (
+                        <div
+                          key={resource._id}
+                          className="bg-black border-b border-l border-purple-800 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow"
+                        >
+                          <h3 className="text-xl font-bold text-white mb-2">{resource.title}</h3>
+                          <p className="text-gray-400 mb-4">{resource.description}</p>
+                          <a
+                            href={resource.link}
+                            className="text-purple-400 hover:text-purple-300 transition-colors"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            View full resource
+                          </a>
+                          <div className="mt-4 flex items-center justify-between">
+                            <span className="inline-block bg-purple-700 text-white text-xs px-2 py-1 rounded">
+                              {resource.category}
+                            </span>
+                            <div className="flex items-center">
+                              <span className="text-white ml-2">
+                                posted by <a href="#" className="text-purple-800">{resource.posterUsername}</a>
+                              </span>
+                              {/* {resource.posterImage && (
+                                <img
+                                  className="rounded-lg w-10 h-10 ml-4"
+                                  src={resource.posterImage}
+                                  alt={resource.posterUsername}
+                                />
+                              )} */}
+                            </div>
+                          </div>
+                        </div>
+                      )
+                    ))}
+                  </div>
                 </div>
               </div>
-            )
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
+            );
+          };
+
 
 export default AllResources;
