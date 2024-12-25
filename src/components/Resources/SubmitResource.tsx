149b20460551aee1cfe58647ef995fed37e650c6
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-12-25 10:18:40 +0530

    upd: userimgurl read only

--- src/components/Resources/SubmitResource.tsx
+++ src/components/Resources/SubmitResource.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FloatingNavbar from "../Section1/Navbar/Navbar";
 import { useUser } from "@clerk/clerk-react";
 import { Link } from "react-router-dom";
+import { Toaster, toast } from "react-hot-toast";
 
 const SubmitResource: React.FC = () => {
   const { user } = useUser();
@@ -12,6 +13,15 @@ const SubmitResource: React.FC = () => {
     category: "",
     posterImage: "",
   });
+
+  useEffect(() => {
+    if (user) {
+      setNewResource((prev) => ({
+        ...prev,
+        posterImage: user.imageUrl || "",
+      }));
+    }
+  }, [user]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -43,6 +53,7 @@ const SubmitResource: React.FC = () => {
         category: "",
         posterImage: user?.imageUrl || "",
       });
+      toast.success("Resource added successfully!");
       console.log("Resource added:", data);
     } catch (error) {
       console.error("Error adding resource:", error);
@@ -58,7 +69,7 @@ const SubmitResource: React.FC = () => {
         </h2>
         <form
           onSubmit={handleSubmit}
-          className="mb-8 bg-gray-800 p-6 rounded-lg shadow-lg"
+          className="mb-8 bg-black border-l-2 border-b  border-purple-800   p-6 rounded-lg shadow-lg"
         >
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <input
@@ -67,7 +78,7 @@ const SubmitResource: React.FC = () => {
               value={newResource.title}
               onChange={handleInputChange}
               placeholder="Title"
-              className="block w-full mb-2 p-2 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-purple-600"
+              className="block w-full mb-2 p-2 rounded  text-white border-purple-800 border-l-2 border-1-2 border-b bg-black focus:outline-none focus:ring-1 focus:ring-purple-600"
               required
             />
             <input
@@ -76,7 +87,7 @@ const SubmitResource: React.FC = () => {
               value={newResource.description}
               onChange={handleInputChange}
               placeholder="Description"
-              className="block w-full mb-2 p-2 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-purple-600"
+              className="block w-full mb-2 p-2 rounded  text-white border-purple-800 border-l-2 border-1-2 border-b bg-black focus:outline-none focus:ring-1 focus:ring-purple-600"
               required
             />
             <input
@@ -85,7 +96,7 @@ const SubmitResource: React.FC = () => {
               value={newResource.link}
               onChange={handleInputChange}
               placeholder="Link"
-              className="block w-full mb-2 p-2 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-purple-600"
+              className="block w-full mb-2 p-2 rounded  text-white border-purple-800 border-l-2 border-1-2 border-b bg-black focus:outline-none focus:ring-1 focus:ring-purple-600"
               required
             />
             <input
@@ -94,33 +105,33 @@ const SubmitResource: React.FC = () => {
               value={newResource.category}
               onChange={handleInputChange}
               placeholder="Category"
-              className="block w-full mb-2 p-2 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-purple-600"
+              className="block w-full mb-2 p-2 rounded  text-white border-purple-800 border-l-2 border-1-2 border-b bg-black focus:outline-none focus:ring-1 focus:ring-purple-600"
             />
             <input
               type="text"
               name="posterImage"
               value={newResource.posterImage}
-              onChange={handleInputChange}
+              readOnly
               placeholder="Poster Image URL"
-              className="block w-full mb-2 p-2 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-purple-600"
+              className="block w-full mb-2 p-2 rounded  text-white border-purple-800 border-l-2 border-1-2 border-b bg-black focus:outline-none focus:ring-1 focus:ring-purple-600"
             />
           </div>
           <div className="buttons flex justify-between">
-          
-          <button
-            type="submit"
-            className="bg-purple-800 text-white p-2 rounded mt-4 hover:bg-purple-700 transition-colors"
+            <button
+              type="submit"
+              className="bg-purple-800 text-white p-2 rounded mt-4 hover:bg-purple-700 transition-colors"
             >
-            Add Resource
-          </button>
-          <Link
-            to="/resources/"
-            className="bg-purple-900 text-white p-2 rounded hover:bg-purple-700 transition-colors"
+              Add Resource
+            </button>
+            <Link
+              to="/resources/"
+              className="bg-purple-900 text-white p-2 rounded hover:bg-purple-700 transition-colors"
             >
-            Back to resource page
-          </Link>
-            </div>
+              Back to resource page
+            </Link>
+          </div>
         </form>
+        <Toaster />
       </div>
     </div>
   );
