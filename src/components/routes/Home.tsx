4d04f18dd8fa348fde5477d88c31750e42e2ab91
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-12-30 22:11:27 +0530

    waiting homepage

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -1,16 +1,16 @@
-import FloatingNavbar from "../Section1/Navbar/Navbar"
-
-
+import { useUser } from "@clerk/clerk-react";
+import FloatingNavbar from "../Section1/Navbar/Navbar";
 const Home = () => {
-    return (
-        
-        <div className="bg-black flex justify-center items-center min-h-screen">
-            <FloatingNavbar/>
-            <div className="text-white text-3xl">
+  const { user } = useUser();
+  return (
+    <div className="bg-black flex justify-center items-center min-h-screen">
+      <FloatingNavbar />
+      <div className="text-white text-3xl">
+       We are under development thanks for waiting 
+        <span className="text-purple-800"> {user?.username}</span>
+      </div>
+    </div>
+  );
+};
 
-                There is nothing for you</div>
-        </div>
-    )
-}
-
-export default Home
+export default Home;
